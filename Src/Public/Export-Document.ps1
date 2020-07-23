e69c74f6133674c95006b0e2a6ac52e33d404e4c
Iain.Brighton <iain.brighton@virtualengine.co.uk>
2020-07-23 11:51:32 +0100

    Adds dynamic -Format parameter to Export-Document

--- Src/Public/Export-Document.ps1
+++ Src/Public/Export-Document.ps1
@@ -9,27 +9,40 @@ function Export-Document {
     param
     (
         ## PScribo document object
-        [Parameter(Mandatory, ValueFromPipeline)]
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
         [System.Management.Automation.PSObject] $Document,
 
-        ## Output formats
-        [Parameter(Mandatory)]
-        [ValidateNotNull()]
-        [System.String[]] $Format,
-
         ## Output file path
-        [Parameter(ValueFromPipelineByPropertyName)]
+        [Parameter(Position = 2, ValueFromPipelineByPropertyName)]
         [ValidateNotNullOrEmpty()]
         [System.String] $Path = (Get-Location -PSProvider FileSystem),
 
         ## PScribo document export option
-        [Parameter(ValueFromPipelineByPropertyName)]
+        [Parameter(Position = 3, ValueFromPipelineByPropertyName)]
         [AllowNull()]
         [System.Collections.Hashtable] $Options,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
+        [Parameter(Position = 4, ValueFromPipelineByPropertyName)]
         [System.Management.Automation.SwitchParameter] $PassThru
     )
+    DynamicParam
+    {
+        ## Adds a dynamic -Format parameter that returns the available plugins
+        $parameterAttribute = New-Object -TypeName 'System.Management.Automation.ParameterAttribute'
+        $parameterAttribute.ParameterSetName = '__AllParameterSets'
+        $parameterAttribute.Mandatory = $true
+        $parameterAttribute.Position = 1
+
+        $attributeCollection = New-Object -TypeName 'System.Collections.ObjectModel.Collection[System.Attribute]'
+        $attributeCollection.Add($parameterAttribute)
+        $validateSetAttribute = New-Object -TypeName 'System.Management.Automation.ValidateSetAttribute' -ArgumentList (Get-PScriboPlugin)
+        $attributeCollection.Add($validateSetAttribute)
+
+        $runtimeParameter = New-Object -TypeName 'System.Management.Automation.RuntimeDefinedParameter' -ArgumentList @('Format', [System.String[]], $attributeCollection)
+        $runtimeParameterDictionary = New-Object -TypeName 'System.Management.Automation.RuntimeDefinedParameterDictionary'
+        $runtimeParameterDictionary.Add('Format', $runtimeParameter)
+        return $runtimeParameterDictionary
+    }
     begin
     {
         try { $Path = Resolve-Path $Path -ErrorAction SilentlyContinue; }
@@ -48,7 +61,7 @@ function Export-Document {
     }
     process
     {
-        foreach ($f in $Format)
+        foreach ($f in $PSBoundParameters['Format'])
         {
             Write-PScriboMessage -Message ($localized.DocumentInvokePlugin -f $f) -Plugin 'Export';
 
