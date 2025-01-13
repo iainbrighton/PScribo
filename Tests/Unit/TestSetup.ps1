b71c11ea75ff52c96f8894724198a80604e49fef
msoukhomlinov <maxs@intellectit.com.au>
2025-01-13 22:07:58 +1100

    Added TestSetup.ps1 for enhanced table tests

---
+++ Tests/Unit/TestSetup.ps1
@@ -0,0 +1,31 @@
+$script:pscriboDocument = Document 'TestDocument' { }
+
+$script:processes = @(
+    [PSCustomObject]@{
+        Name = "TestProcess1"
+        Id = 1
+        CPU = 10
+        Memory = 100MB
+    },
+    [PSCustomObject]@{
+        Name = "TestProcess2"
+        Id = 2
+        CPU = 20
+        Memory = 200MB
+    }
+)
+
+$script:services = @{
+    Service1 = [PSCustomObject]@{
+        Name = "TestService1"
+        Status = "Running"
+        StartType = "Automatic"
+    }
+    Service2 = [PSCustomObject]@{
+        Name = "TestService2"
+        Status = "Stopped"
+        StartType = "Manual"
+    }
+}
+
+Export-ModuleMember -Variable pscriboDocument, processes, services 
